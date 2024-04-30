8a7fe0b17d9f814bedb0dd638031296134f5b6f5
Ringo De Smet <ringo@de-smet.name>
2024-04-29 22:58:14 -1000

    Update the logo to PNG

--- provider/resources.go
+++ provider/resources.go
@@ -67,7 +67,7 @@ func Provider() tfbridge.ProviderInfo {
 		//
 		// You may host a logo on a domain you control or add an SVG logo for your package
 		// in your repository and use the raw content URL for that file as your logo URL.
-		LogoURL: "https://assets.website-files.com/5de9972f49103c5df3964004/5f3ef8ee5f2763121f8bc9ad_Logo%20Dark.svg",
+		LogoURL: "https://raw.githubusercontent.com/pulumiverse/pulumi-doppler/main/docs/logo.png",
 		// PluginDownloadURL is an optional URL used to download the Provider
 		// for use in Pulumi programs
 		// e.g https://github.com/org/pulumi-provider-name/releases/
